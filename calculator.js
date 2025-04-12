ef6636e6e08a687daba7706531b0f1e1b4813c2b
Michael Juliano <mdj.michaeljuliano@gmail.com>
2025-04-12 11:49:06 -0400

    Create function in js to create calculator buttons and add them to buttons div

--- calculator.js
+++ calculator.js
@@ -13,3 +13,53 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b; 
 };
+
+let numberOfButtons = 19;
+
+for (let i = 1; i < numberOfButtons; i++) {
+   let myButtons = document.createElement('button');
+   myButtons.classList.add('myButtons');
+
+   if (i === 1) {
+    myButtons.textContent = "AC";
+   } else if (i === 2) {
+    myButtons.textContent = "+/-";
+   }
+     else if (i === 3) {
+        myButtons.textContent = "%";
+     }
+     else if (i === 4) {
+        myButtons.textContent = "/";
+     }
+     else if (i > 4 && i <= 7) {
+        myButtons.textContent = `${i+2}`;
+     }
+     else if (i === 8) {
+        myButtons.textContent = "*";
+     }
+     else if (i > 8 && i <= 11) {
+        myButtons.textContent = `${i-5}`;
+     }
+     else if (i === 12) {
+        myButtons.textContent = "-";
+     }
+     else if (i > 12 && i <= 15) {
+        myButtons.textContent = `${i-12}`;
+     }
+     else if (i === 16) {
+        myButtons.textContent = "+";
+     }
+     else if (i === 17) {
+        myButtons.textContent = `${i-17}`;
+     }
+     else if (i === 18) {
+        myButtons.textContent = ".";
+     }
+     else if (i === 19) {
+        myButtons.textContent = "=";
+     }
+
+     document.getElementById('buttons').appendChild(myButtons);
+
+
+}
